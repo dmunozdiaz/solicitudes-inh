fcbf7ebeedcdb6fef8ab54b8a3d9ce907f960312
Diego Muñoz Diaz <dmunozdiaz@gmail.com>
2026-08-25 12:19:27 -0400

    prueba

--- resources/views/welcome-publico.blade.php
+++ resources/views/welcome-publico.blade.php
@@ -20,8 +20,8 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" />
     <!--end::Fonts-->
     <!--begin::Global Stylesheets Bundle(used by all pages)-->
-    <link href="/assets/plugins/global/plugins.bundle.css" rel="stylesheet" type="text/css" />
-    <link href="/assets/css/style.bundle.css" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('acceso-solicitudes/public/assets/plugins/global/plugins.bundle.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('acceso-solicitudes/public/assets/css/style.bundle.css') }}" rel="stylesheet" type="text/css" />
 
     <link href="{{ asset('css/cu.min.css') }}" rel="stylesheet" type="text/css" />
     <!--end::Global Stylesheets Bundle-->
@@ -132,11 +132,11 @@
         var hostUrl = "/assets/";
     </script>
     <!--begin::Global Javascript Bundle(used by all pages)-->
-    <script src="/assets/plugins/global/plugins.bundle.js"></script>
-    <script src="/assets/js/scripts.bundle.js"></script>
+    <script src="{{ asset('acceso-solicitudes/public/assets/plugins/global/plugins.bundle.js') }}"></script>
+    <script src="{{ asset('acceso-solicitudes/public/assets/js/scripts.bundle.js') }}"></script>
     <!--end::Global Javascript Bundle-->
     <!--begin::Page Custom Javascript(used by this page)-->
-    <script src="/assets/js/custom/authentication/sign-up/general.js"></script>
+    <script src="{{ asset('acceso-solicitudes/public/assets/js/custom/authentication/sign-up/general.js') }}"></script>
     <!--end::Page Custom Javascript-->
     <!--end::Javascript-->
 </body>
